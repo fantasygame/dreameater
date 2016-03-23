7fabcc0b7a88d5ec84f2b794f00a38d521056d72
gogi <grobelska.m@gmail.com>
2016-03-23 21:03:39 +0100

    decorate dream for view 'show'

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -77,7 +77,7 @@ class DreamsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_dream
-      @dream = Dream.find(params[:id])
+      @dream = Dream.find(params[:id]).decorate
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
