b9632484ec004bc41f91733aaffe31d8337abc88
gogi <grobelska.m@gmail.com>
2016-03-23 19:27:12 +0100

    Fixed user name acquisition for devise and resolved conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,22 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  include Pundit
   protect_from_forgery with: :exception
+
+  include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 end
