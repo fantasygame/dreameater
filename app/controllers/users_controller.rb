699420a1fe055d9640191866f9ed86d0351372f1
Jakub Kubacki <kubacki.jk@gmail.com>
2016-03-23 19:48:17 +0100

    Add option to unfollow user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
     current_user.follow(user_to_follow)
     redirect_to :back, notice: "You are stalking #{user_to_follow.name}"
   end
+
+  def unstalk
+    user_to_unfollow = User.find(params[:user_id])
+    current_user.stop_following(user_to_unfollow)
+    redirect_to :back, notice: "You are not stalking #{user_to_unfollow.name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :dreams
   resources :users, only: [] do
     get :stalk
+    get :unstalk
   end
 
   root to: 'visitors#index'
