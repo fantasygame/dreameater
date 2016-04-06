3850247a48fae62f1d59603d061ba72c425c79ac
Jakub Kubacki <kubacki.jk@gmail.com>
2016-04-06 17:50:26 +0200

    Update navigation spec for logged in user

--- spec/features/visitors/navigation_spec.rb
+++ spec/features/visitors/navigation_spec.rb
@@ -1,17 +1,45 @@
-# Feature: Navigation links
-#   As a visitor
-#   I want to see navigation links
-#   So I can find home, sign in, or sign up
-feature 'Navigation links', :devise do
+feature "Navigation links", :devise do
 
-  # Scenario: View navigation links
-  #   Given I am a visitor
-  #   When I visit the home page
-  #   Then I see "home," "sign in," and "sign up"
-  scenario 'view navigation links' do
-    visit root_path
-    expect(page).to have_content 'Dreameater'
-    expect(page).to have_content 'Sign in'
-    expect(page).to have_content 'Sign up'
+  context "user not signed in" do
+    before { visit root_path }
+
+    scenario "view signin and signup links" do
+      expect(page).to have_content 'Dreameater'
+      expect(page).to have_content 'Sign in'
+      expect(page).to have_content 'Sign up'
+    end
+
+    scenario "doesn't view full navigation" do
+      expect(page).not_to have_content 'Dreams'
+      expect(page).not_to have_content 'Stalked dreams'
+      expect(page).not_to have_content 'My dreams'
+      expect(page).not_to have_content 'Top dreams'
+      expect(page).not_to have_content 'Edit account'
+      expect(page).not_to have_content 'Sign out'
+    end
+  end
+
+  context "user signed in" do
+    before do
+      user = FactoryGirl.create(:user, name: 'Dziniu')
+      signin(user.email, user.password)
+      visit root_path
+    end
+
+    scenario "view signed user navigation links" do
+      expect(page).to have_content 'Dreameater'
+      expect(page).to have_content 'Dreams'
+      expect(page).to have_content 'Stalked dreams'
+      expect(page).to have_content 'My dreams'
+      expect(page).to have_content 'Top dreams'
+      expect(page).to have_content 'Dziniu'
+      expect(page).to have_content 'Edit account'
+      expect(page).to have_content 'Sign out'
+    end
+
+    scenario "doesn't view signin and signup" do
+      expect(page).not_to have_content 'Sign in'
+      expect(page).not_to have_content 'Sign up'
+    end
   end
 end
